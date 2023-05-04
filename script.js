a427c72ab6689fc9adf16c14c77421134f0ee598
Neeraj Nishad <neerajnishad5@gmail.com>
2023-05-04 12:04:18 +0530

    added date

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-(async function getMatchData(){
+(async function getMatchData() {
   try {
     console.log("I am called");
     // calling api
     const data = await fetch(
-      "https://api.cricapi.com/v1/currentMatches?apikey=bf7f406c-013c-4f06-8253-cb7311631320&offset=0"
+      "https://api.cricapi.com/v1/currentMatches?apikey=fe7fe256-39c4-44f5-a7a3-a4967d168e9d&offset=0"
     );
 
     // getting json data
@@ -28,22 +28,21 @@
         if (match.score.length === 1) {
           battingTeam = match.score[0].inning;
           battingTeam = battingTeam.slice(0, -9);
-          
         } else {
           battingTeam = match.score[1].inning;
           battingTeam = battingTeam.slice(0, -9);
-          
         }
         if (match.matchEnded) {
-          return `<p><img src=${match.teamInfo[0].img} alt="team-logo"> ${match.teamInfo[0].shortname} vs. ${match.teamInfo[1].shortname} <img src=${match.teamInfo[1].img} alt="team-logo"></p>
+          return `<div>
+          <div><p>On ${match.date.split("-").reverse().join(".")}</p></div>
+          <div><p><img src=${match.teamInfo[0].img} alt="team-logo"> ${match.teamInfo[0].shortname} vs. ${match.teamInfo[1].shortname} <img src=${match.teamInfo[1].img} alt="team-logo"></p></div>
                 
-          <span>Result:</span> <span class="result">${match.status}</span>`;
+          <div><span>Result:</span> <span class="result">${match.status}</span> </div> </div>`;
         } else if (!match.matchEnded) {
           return `<p><img src=${match.teamInfo[0].img} alt="team-logo"> ${match.teamInfo[0].shortname} vs. ${match.teamInfo[1].shortname} <img src=${match.teamInfo[1].img} alt="team-logo"></p>
            
         <p>${battingTeam}: ${match.score[0]?.r} - ${match.score[0]?.w}</p>
-        <p>Status: ${match.status}</p>`
-        ;
+        <p>Status: ${match.status}</p>`;
         }
       }
     });
@@ -59,6 +58,6 @@
   } catch (error) {
     console.log(error);
   }
-}) ();
+})();
 
 // getMatchData();
